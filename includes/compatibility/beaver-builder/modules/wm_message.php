56842584115ce19d5f6e892cbe579e0cc79e0074
Oliver Juhas | WebMan Design <webmandesigneu@gmail.com>
2017-11-10 21:58:42 +0100

    1.6.0

--- includes/compatibility/beaver-builder/modules/wm_message.php
+++ includes/compatibility/beaver-builder/modules/wm_message.php
@@ -10,30 +10,6 @@
  * @since    1.1.0
  * @version  1.6.0
  */
-class WM_Amplifier_Beaver_Builder_Module_Message extends WM_Amplifier_Beaver_Builder_Register_Module {
-
-
-
-
-
-	/**
-	 * Constructor
-	 *
-	 * @since    1.1.0
-	 * @version  1.6.0
-	 */
-	public function __construct() {
-
-		// Processing
-
-			parent::__construct();
-
-	} // /__construct
-
-
-
-
-
-} // /WM_Amplifier_Beaver_Builder_Module_Message
+class WM_Amplifier_Beaver_Builder_Module_Message extends WM_Amplifier_Beaver_Builder_Register_Module {}
 
 WM_Amplifier_Beaver_Builder_Module_Message::register( __FILE__ );
